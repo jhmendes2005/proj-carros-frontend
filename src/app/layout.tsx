b55e8bc75c793f383c042022dbf7d32f1fbfbd9c
jhmendes2005 <jhmendes2005@gmail.com>
2025-12-01 22:04:58 -0300

    first2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+// --- Import CSS ---
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "./globals.css";
 
 // --- Importações dos Componentes ---
-// Certifique-se de que os nomes dos arquivos correspondem (ex: footer.tsx vs Footer.tsx)
 import Header from '@/src/components/header';
 import FloatingWhatsApp from "@/src/components/FloatingWhatsApp"; 
 import AnalyticsTracker from "@/src/components/AnalyticsTracker";
@@ -39,7 +43,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
-        {/* Envolvemos toda a aplicação com o Provider para o estado do modal funcionar em qualquer lugar */}
+        {/* Provider para estado do modal funcionar em qualquer lugar */}
         <WhatsAppProvider>
           
           {/* Analytics roda invisível no topo */}
@@ -53,7 +57,7 @@ export default function RootLayout({
 
           <Footer />
           
-          {/* O botão flutuante fica aqui para sobrepor tudo */}
+          {/* Botões flutuantes */}
           <AIChatButton />
           <FloatingWhatsApp />
           
